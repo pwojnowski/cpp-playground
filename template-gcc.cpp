376a83394acea65091765e7baacfaf5441cdcbd0
Przemysław Wojnowski <esperanto@defun.tech>
2024-10-16 19:08:24 +0200

    Add file templates for GNU C++ and Std

---
+++ template-gcc.cpp
@@ -0,0 +1,7 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  cout << "Write your code here!\n";
+}
---
+++ template-std.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  cout << "Write your code here!\n";
+}
